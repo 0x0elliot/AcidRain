bc6fb4bb4e648555657460c87f6674b4df445fec
Aditya <60684641+0x0elliot@users.noreply.github.com>
2024-06-12 02:52:21 +0530

    init: starting the project

---
+++ frontend/src/app/(main)/overview/page.tsx
@@ -0,0 +1,152 @@
+"use client"
+import { CategoryBarCard } from "@/components/ui/overview/DashboardCategoryBarCard"
+import { ChartCard } from "@/components/ui/overview/DashboardChartCard"
+import { Filterbar } from "@/components/ui/overview/DashboardFilterbar"
+import { ProgressBarCard } from "@/components/ui/overview/DashboardProgressBarCard"
+import { overviews } from "@/data/overview-data"
+import { OverviewData } from "@/data/schema"
+import { cx } from "@/lib/utils"
+import { subDays, toDate } from "date-fns"
+import React from "react"
+import { DateRange } from "react-day-picker"
+
+export type PeriodValue = "previous-period" | "last-year" | "no-comparison"
+
+const categories: {
+  title: keyof OverviewData
+  type: "currency" | "unit"
+}[] = [
+  {
+    title: "Rows read",
+    type: "unit",
+  },
+  {
+    title: "Rows written",
+    type: "unit",
+  },
+  {
+    title: "Queries",
+    type: "unit",
+  },
+  {
+    title: "Payments completed",
+    type: "currency",
+  },
+  {
+    title: "Sign ups",
+    type: "unit",
+  },
+  {
+    title: "Logins",
+    type: "unit",
+  },
+]
+
+export type KpiEntry = {
+  title: string
+  percentage: number
+  current: number
+  allowed: number
+  unit?: string
+}
+
+const data: KpiEntry[] = [
+  {
+    title: "Rows read",
+    percentage: 48.1,
+    current: 48.1,
+    allowed: 100,
+    unit: "M",
+  },
+  {
+    title: "Rows written",
+    percentage: 78.3,
+    current: 78.3,
+    allowed: 100,
+    unit: "M",
+  },
+  {
+    title: "Storage",
+    percentage: 26,
+    current: 5.2,
+    allowed: 20,
+    unit: "GB",
+  },
+]
+
+const data2: KpiEntry[] = [
+  {
+    title: "Weekly active users",
+    percentage: 21.7,
+    current: 21.7,
+    allowed: 100,
+    unit: "%",
+  },
+  {
+    title: "Total users",
+    percentage: 70,
+    current: 28,
+    allowed: 40,
+  },
+  {
+    title: "Uptime",
+    percentage: 98.3,
+    current: 98.3,
+    allowed: 100,
+    unit: "%",
+  },
+]
+
+export type KpiEntryExtended = Omit<
+  KpiEntry,
+  "current" | "allowed" | "unit"
+> & {
+  value: string
+  color: string
+}
+
+const data3: KpiEntryExtended[] = [
+  {
+    title: "Base tier",
+    percentage: 68.1,
+    value: "$200",
+    color: "bg-indigo-600 dark:bg-indigo-500",
+  },
+  {
+    title: "On-demand charges",
+    percentage: 20.8,
+    value: "$61.1",
+    color: "bg-purple-600 dark:bg-indigo-500",
+  },
+  {
+    title: "Caching",
+    percentage: 11.1,
+    value: "$31.9",
+    color: "bg-gray-400 dark:bg-gray-600",
+  },
+]
+
+const overviewsDates = overviews.map((item) => toDate(item.date).getTime())
+const maxDate = toDate(Math.max(...overviewsDates))
+
+export default function Overview() {
+  const [selectedDates, setSelectedDates] = React.useState<
+    DateRange | undefined
+  >({
+    from: subDays(maxDate, 30),
+    to: maxDate,
+  })
+
+  return (
+    <>
+      <section aria-labelledby="flows-title">
+        <h1
+          id="current-billing-cycle"
+          className="scroll-mt-10 text-lg font-semibold text-gray-900 sm:text-xl dark:text-gray-50"
+        >
+          Your WhatsApp Flows
+        </h1>
+      </section>
+    </>
+  )
+}
