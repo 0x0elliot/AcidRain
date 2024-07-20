26f2f4ebf75c147bc1be902ac20627da6a803342
Aditya <60684641+0x0elliot@users.noreply.github.com>
2024-07-20 06:06:42 +0530

    fix: more progress on tracking

--- backend/router/tracking.go
+++ backend/router/tracking.go
@@ -11,22 +11,21 @@ import (
 
 func SetupTrackingRoutes() {
 	// set up
-	tracking := TRACKING.Group("/tracking")
-	tracking.Post("/sync", HandleTrackedUserSync)
+	TRACKING.Post("/sync", HandleTrackedUserSync)
 }
 
 // add as less validation here as possible by design
 // be open to all blocking edge cases. This is just a plain WRITE APIs and if else conditions.
 // that's how they rule the world. We will too.
-// "Do not let the hero in your soul perish, in lonely frustration for 
+// "Do not let the hero in your soul perish, in lonely frustration for
 // the life you deserved, but have never been able to reach. The world you desire
 // can be won. It exists.. It is real.. it is possible..
 // It's yours." - Ayn Rand"
 func HandleTrackedUserSync(c *fiber.Ctx) error {
 	type FingerPrint struct {
-		FingerPrint string `json:"fingerprint"`
-		ShopifyUniqueID string `json:"shopify_unique_id"`
-		Store string `json:"store"`
+		FingerPrint                  string `json:"fingerprint"`
+		ShopifyUniqueID              string `json:"shopify_unique_id"`
+		Store                        string `json:"store"`
 		PushNotificationSubscription string `json:"push_notification_subscription"`
 	}
 
@@ -34,8 +33,8 @@ func HandleTrackedUserSync(c *fiber.Ctx) error {
 	if err := c.BodyParser(newFingerPrint); err != nil {
 		log.Printf("[ERROR] Error parsing request body: %v", err)
 		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
-			"error": true,
-			"message":   "Error parsing request body",
+			"error":   true,
+			"message": "Error parsing request body",
 		})
 	}
 
@@ -65,16 +64,14 @@ func HandleTrackedUserSync(c *fiber.Ctx) error {
 	if err != nil {
 		log.Printf("[ERROR] Error in saving fingerprint: %v", err)
 		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
-			"error": true,
-			"message":   "Error saving fingerprint",
+			"error":   true,
+			"message": "Error saving fingerprint",
 			"user_id": userId,
 		})
 	}
 
 	return c.Status(fiber.StatusOK).JSON(fiber.Map{
-		"error": false,
+		"error":   false,
 		"message": "Fingerprint saved successfully",
 	})
 }
-
-
